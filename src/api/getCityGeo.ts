1b360a920a14fec30cd44e254ce423e6865e8a14
erive92d <erive92d@gmail.com>
2025-01-26 05:44:13 -0800

    result input https

--- src/api/getCityGeo.ts
+++ src/api/getCityGeo.ts
@@ -1,7 +1,7 @@
 export default async function getCityGeo(cityName: string) {
 
     try {
-        const getData = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${import.meta.env.VITE_WEATHER_API}`)
+        const getData = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${import.meta.env.VITE_WEATHER_API}`)
         if (!getData.ok) {
             console.log(getData)
         }
